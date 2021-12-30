897992287b56cb685a998e418d7ff56a8c8c76ac
Devin Mullin <devin.mullin90@gmail.com>
2021-12-30 17:59:19 -0600

    added more spheres and textures

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import Player from './Player'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import vwave from './pics/vwave.jpg'
+import medusa from './pics/medusa.png'
+import test from './pics/test.jpg'
+import sun from './pics/sun.png'
 
 
 function App() {
@@ -36,12 +39,10 @@ function App() {
 // ];
   
 
-
-  const geometry = new THREE.TetrahedronGeometry(1.5, 0)
+  const polyTexture = new THREE.TextureLoader().load(medusa)
+  const geometry = new THREE.SphereGeometry(3, 26, 26)
   const material = new THREE.MeshStandardMaterial( { 
-    color: 0xf800b5, 
-    roughness: 0.1,
-    metalness: 0.1
+    map: polyTexture
   } )
   const poly = new THREE.Mesh( geometry, material )
   
@@ -50,8 +51,27 @@ function App() {
   poly.position.setX(-10)
   camera.position.z = 10
 
+  const polyTexture2 = new THREE.TextureLoader().load(test)
+  const material2 = new THREE.MeshStandardMaterial( { 
+    map: polyTexture2
+  } )
+  const poly2 = new THREE.Mesh( geometry, material2 )
+  
+  scene.add(poly2)
+  poly2.position.z = 25
+  poly2.position.setX(-10)
 
-   
+  const polyTexture3 = new THREE.TextureLoader().load(sun)
+  const material3 = new THREE.MeshStandardMaterial( { 
+    map: polyTexture3
+  } )
+  const poly3 = new THREE.Mesh( geometry, material3 )
+  
+  scene.add(poly3)
+  poly3.position.z = 45
+  poly3.position.setX(-10)
+
+
 
   const pointLight = new THREE.PointLight(0xffffff)
   pointLight.position.set(10, 10, 10)
@@ -66,7 +86,11 @@ function App() {
 
   function newStar(){
     const geometry = new THREE.SphereGeometry(0.25, 24, 24)
-    const material = new THREE.MeshStandardMaterial( { color: 0xffffff })
+    const material = new THREE.MeshStandardMaterial( { 
+      color: 0x00ffff, 
+      roughness: 0.1,
+      metalness: 0.4
+    })
     const star = new THREE.Mesh( geometry, material )
     const [x, y, z] = Array(3).fill().map(()=> THREE.MathUtils.randFloatSpread( 100 ))
     
@@ -85,7 +109,12 @@ function App() {
     poly.rotation.x += 0.1
     poly.rotation.y += 0.125
     poly.rotation.z += 0.125
-
+    poly2.rotation.x += 0.1
+    poly2.rotation.y += 0.125
+    poly2.rotation.z += 0.125
+    poly3.rotation.x += 0.2
+    poly3.rotation.y += 0.225
+    poly3.rotation.z += 0.2
     camera.position.z = top * -0.1
     camera.position.x = top * -0.02
     camera.rotation.y = top * -0.02
@@ -98,6 +127,12 @@ function App() {
     poly.rotation.x += 0.05
     poly.rotation.y += 0.005
     poly.rotation.z += 0.05
+    poly2.rotation.x += -0.05
+    poly2.rotation.y += -0.005
+    poly2.rotation.z += -0.05
+    poly3.rotation.x += 0.05
+    poly3.rotation.y += 0.005
+    poly3.rotation.z += 0.05
 
     controls.update()
 
